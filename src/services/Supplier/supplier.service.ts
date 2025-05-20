6f6cfb1d822df201439933f2d1830ba6940b95b1
Bao Do <baodo.dev@gmail.com>
2025-05-20 15:16:25 +0700

    Supplier API Calling

---
+++ src/services/Supplier/supplier.service.ts
@@ -0,0 +1,62 @@
+import axios from 'axios';
+
+export const getSuppliers = async (
+    page = 1,
+    limit = 5,
+    search = '',
+    accessToken: string | null = null
+  ) => {
+    if (!accessToken) {
+      throw new Error('No access token provided');
+    }
+  
+    const response = await axios.get('http://localhost:3000/suppliers', {
+      params: {
+        page,
+        limit,
+        search,
+      },
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  
+    return response.data;
+  };
+
+  export const createSupplier = async (categoryData: any, accessToken: string) => {
+    const response = await axios.post('http://localhost:3000/suppliers', categoryData, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+    return response.data;
+};
+
+export const getSupplierById = async (id: string, accessToken: string) => {
+    const response = await axios.get(`http://localhost:3000/suppliers/${id}`, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+    return response.data;
+};
+
+export const updateSupplier = async (id: string, supplierData: any, accessToken: string) => {
+    const response = await axios.put(`http://localhost:3000/suppliers/${id}`, supplierData, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+    return response.data;
+};
+
+export const deleteSupplier = async (id: string, accessToken: string) => {
+    const response = await axios.delete(`http://localhost:3000/suppliers/${id}`, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+    return response.data;
+};          
+
